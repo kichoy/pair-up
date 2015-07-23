8558e838dc2f745d0a0914c0d325a26779ce4a28
kichoy <kichoy@s.sfusd.edu>
2015-07-23 21:13:00 +0000

    Add model User
    Has two methods, hasPartner() and update(). Using array to store list of
    students (ie cohort), and array to store list of cohort.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-// fun mess around functionality to button
+// Animation
+// =================
 function changeText() {
 	var sloganObj = document.getElementById('slogan');
 	var findButton = document.getElementById('find-button');
@@ -10,3 +11,61 @@ function changeText() {
 		findButton.value = "Yes, I'm looking for a partner!";
 	}
 }
+
+
+
+
+// Models
+// =================
+function User (username, partner, cohort) {
+  this.username = username;
+  this.partner = partner; // boolean
+  this.cohort = cohort;
+}
+
+User.prototype.hasPartner = function () {
+  return (this.partner);
+};
+
+User.prototype.update = function (username, partner, cohort) {
+
+  // update properties
+  this.username = username;
+  this.partner = partner;
+  this.cohort = cohort;
+  
+  cohort1.push(this);
+};
+
+// cohort - list of users
+var cohort1 = [];
+
+// list of cohorts
+var cohortList = [];
+cohortList.push(cohort1);
+
+
+// // List of students in a cohort
+// function UserList (cohort, userList) {
+//   this.cohort = cohort; // number
+//   this.userList = userList; // array
+// }
+
+// UserList.prototype.update = function (userList) {
+//   this.userList = userList;
+// };
+
+
+// Testing
+// create user list (aka cohort)
+function createTestData (amount) {
+  for (var i = 0; i < amount; i++) {
+    cohort1.push(new User(
+      "User" + i,
+      false,
+      1
+    ));
+  }
+}
+
+createTestData(5);
