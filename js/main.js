822feb1c7ea0961d62aa9b300a35330fa727bdb3
kichoy <kichoy@s.sfusd.edu>
2015-07-30 23:36:00 +0000

    Integrate firebase
    User data and cohort data stored in firebase
    Data is also fetched from firebase. Working system

--- js/main.js
+++ js/main.js
@@ -1,94 +1,163 @@
-// Animation
-// =================
-function changeText() {
-	var sloganObj = document.getElementById('slogan');
-	var findButton = document.getElementById('find-button');
-	if (sloganObj.innerHTML == "Do you want us to pair you? (Random)") {
-		sloganObj.innerHTML = "Partner found: **** ******";
-		findButton.value = "New Partner";
-	} else {
-		sloganObj.innerHTML = "Do you want us to pair you? (Random)";
-		findButton.value = "Yes, I'm looking for a partner!";
-	}
-}
-
-
-
-
-// Initialize
-// =================
-var app = { model:{}, view:{}, controller:{} }; 
-
-
-
-
-// Models
-// =================
-function User (username, partner, cohort, photo) {
-  this.username = username;
-  this.partner = partner; // boolean
-  this.cohort = cohort;
-  this.photo = photo; // string link
-}
-
-User.prototype.update = function (username, partner, photo, cohort) {
-
-  // update properties
-  this.username = username;
-  this.partner = partner;
-  this.photo = photo;
-  this.cohort = cohort;
+$(document).ready(function () {
   
-  cohort.push(this);
-};
-
-// cohort - list of users
-var cohort1 = [];
-
-// list of cohorts
-var cohortList = [];
-cohortList.push(cohort1);
-
-// ### Use object instead of array? ###
-// // List of students in a cohort
-// function UserList (cohort, userList) {
-//   this.cohort = cohort; // number
-//   this.userList = userList; // array
-// }
-
-// UserList.prototype.update = function (userList) {
-//   this.userList = userList;
-// };
-
-
-// Testing
-// create user list (aka cohort)
-function createTestData (amount) {
-  for (var i = 0; i < amount; i++) {
-    cohort1.push(new User(
-      "User" + i,
-      false,
-      1,
-      "https://icons.iconarchive.com/icons/seanau/user/128/Thief-icon.png"
-    ));
-  }
-}
-
-createTestData(15);
-
-
-
-
-// View
-// =================
-app.view.displayUsers = function (users) {
   
-  // get the unordered list 
-  var list = document.getElementById("user-list");
+  // Firebase
+  // =================
+  var firebaseRef = new Firebase("https://pair-up.firebaseio.com/");
+  var usersRef = firebaseRef.child("users");
+  var groupsRef = firebaseRef.child("groups");
   
-  for (var i = 0; i < users.length; i++) {
-    // create li, div, p, and img
+  // groupsRef.set({
+  //   cohort1: {
+  //     number: 1,
+  //     members: {}
+  //   },
+  //   cohort2: {
+  //     number: 2,
+  //     members: {}
+  //   }
+  // });
+  
+  usersRef.on('child_added', add);
+  
+  function add (idSnapshot) {
+    // when an item is added to the index, fetch the data
+    var user = idSnapshot.val();
     
+    // display user on list
+    displayUser(user);
+  }
+  
+  function getCohort (cohortNumber) {
+    // fetch users by cohort
+  }
+  
+  
+  // Animation
+  // =================
+  function changeText () {
+    var sloganObj = document.getElementById('slogan');
+    var findButton = document.getElementById('find-button');
+    if (sloganObj.innerHTML == "Do you want us to pair you? (Random)") {
+      sloganObj.innerHTML = "Partner found: **** ******";
+      findButton.value = "New Partner";
+    } else {
+      sloganObj.innerHTML = "Do you want us to pair you? (Random)";
+      findButton.value = "Yes, I'm looking for a partner!";
+    }
+  }
+  
+  
+  
+  
+  // Models
+  // =================
+  function User (username, partner, cohort, photo) {
+    this.username = username;
+    this.partner = partner; // boolean
+    this.cohort = cohort;
+    this.photo = photo; // string link
+  }
+  
+  User.prototype.update = function (username, partner, photo, cohort) {
+  
+    // update properties
+    this.username = username;
+    this.partner = partner;
+    this.photo = photo;
+    this.cohort = cohort;
+    
+    cohort.push(this);
+  };
+
+  function Group (cohortNumber, members) {
+    this.cohortNumber = cohortNumber;
+    this.members = members;
+  }
+  
+  // Push username (not user) into group object
+  // do not push entire user object bc that would be excessive and redundant 
+  Group.prototype.push = function (username) {
+    var membersArray = this.members;
+    membersArray.push(username);
+    this.members = membersArray;
+  };
+  
+  
+  // ### Testing ###
+  var cohort1; // cohort - list of users
+  usersRef.set({}); // clear db 
+  
+  // create user list (aka cohort)
+  function createTestData (amount) {
+    cohort1 = new Group (1, []); 
+    
+    for (var i = 0; i < amount; i++) {
+      var user = new User (
+        "User" + i,
+        "",
+        1,
+        "https://icons.iconarchive.com/icons/seanau/user/128/Thief-icon.png"
+      );
+      cohort1.push(user.username);
+      usersRef.child(user.username).set(user); // create user object
+      console.log(user);
+    }
+    
+    groupsRef.set({
+      cohort1,
+      cohort2: {
+        cohortNumber: 2,
+        members: ['jack', 'jill']
+      }
+    }); // sets group object with array of usernames
+  }
+  createTestData(5);
+  
+  
+  
+  
+  // View
+  // =================
+  function displayUsers (users) {
+    
+    // get the unordered list 
+    var list = document.getElementById("user-list");
+    
+    for (var i = 0; i < users.length; i++) {
+      // create li, div, p, and img
+      
+      var listItem = document.createElement("li");
+      var userContainer = document.createElement("div");
+      userContainer.className = "user-display-block";
+      
+      
+      // username is <p> so needs text node 
+      var username = document.createElement("p");
+      username.appendChild(document.createTextNode(users[i].username));
+    
+      var img = document.createElement("img");
+      img.src = users[i].photo;
+      img.alt = users[i].username;
+      
+      // append img <li> and username <p> to container <div>
+      userContainer.appendChild(img);
+      userContainer.appendChild(username);
+      
+      // append to the list item <li>
+      listItem.appendChild(userContainer);
+    
+      // append list item to the <ul>
+      list.appendChild(listItem);
+    }
+  
+  };
+  
+  function displayUser (user) {
+    // get the unordered list 
+    var list = document.getElementById("user-list");
+    
+    // create li, div, p, and img
     var listItem = document.createElement("li");
     var userContainer = document.createElement("div");
     userContainer.className = "user-display-block";
@@ -96,11 +165,11 @@ app.view.displayUsers = function (users) {
     
     // username is <p> so needs text node 
     var username = document.createElement("p");
-    username.appendChild(document.createTextNode(users[i].username));
+    username.appendChild(document.createTextNode(user.username));
   
     var img = document.createElement("img");
-    img.src = users[i].photo;
-    img.alt = users[i].username;
+    img.src = user.photo;
+    img.alt = user.username;
     
     // append img <li> and username <p> to container <div>
     userContainer.appendChild(img);
@@ -110,10 +179,17 @@ app.view.displayUsers = function (users) {
     listItem.appendChild(userContainer);
   
     // append list item to the <ul>
-    list.appendChild(listItem);
+    list.appendChild(listItem);    
   }
-
-};
+  
+  
+  
+  
+  // Load users
+  // =================
+  // app.view.displayUsers(cohortArray);
+  
+});
 
 
 
